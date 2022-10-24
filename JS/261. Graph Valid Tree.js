6301d84485d39176c8d1a35a2ccde06843d1301e
Justin Lawrence <justinmarklawrence55@gmail.com>
2022-10-23 17:17:46 -0700

    fix: cleanup numbering

---
+++ JS/261. Graph Valid Tree.js
@@ -0,0 +1,57 @@
+// 261. Graph Valid Tree
+
+// You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges where edges[i] = [ai, bi] indicates that there is an undirected edge between nodes ai and bi in the graph.
+
+// Return true if the edges of the given graph make up a valid tree, and false otherwise.
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {boolean}
+ */
+var validTree = function (n, edges) {
+  // Sanity check n is > 0
+  if (!n) {
+    return false;
+  }
+
+  // Set up adjacency table
+  // <nodes> : <nodes>
+  const adjacency = {};
+
+  for (let i = 0; i < n; i++) {
+    adjacency[i] = [];
+  }
+
+  // Note: we are marking adjacency both ways
+  for (const [start, end] of edges) {
+    adjacency[start].push(end);
+    adjacency[end].push(start);
+  }
+
+  let visited = new Set();
+
+  const dfs = (currentNode, prev) => {
+    if (visited.has(currentNode)) {
+      return false;
+    }
+
+    visited.add(currentNode);
+
+    for (const adjacentNode of adjacency[currentNode]) {
+      // We don't want to check this
+      if (adjacentNode === prev) {
+        continue;
+      }
+
+      // We want to check these
+      if (!dfs(adjacentNode, currentNode)) {
+        return false;
+      }
+    }
+
+    return true;
+  };
+
+  // Check both: no cycles and no islands
+  return dfs(0, -1) && n === visited.size;
+};
--- JS/424. Longest Repeating Character Replacement.js
+++ JS/424. Longest Repeating Character Replacement.js
@@ -1,25 +1,26 @@
 // 424. Longest Repeating Character Replacement
+
+// -------------------------------------------
+
 // You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
 
 // Return the length of the longest substring containing the same letter you can get after performing the above operations.
 
-// Example 1:
+// ------------
 
 // Input: s = "ABAB", k = 2
 // Output: 4
 // Explanation: Replace the two 'A's with two 'B's or vice versa.
-// Example 2:
 
-// Input: s = "AABABBA", k = 1
-// Output: 4
-// Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
-// The substring "BBBB" has the longest repeating letters, which is 4.
+// -------------------------------------------
+
 /**
  * @param {string} s
  * @param {number} k
  * @return {number}
  */
 var characterReplacement = function (s, k) {
+  // 4
   const count = {};
   let left = 0;
   let maxF = 0;
